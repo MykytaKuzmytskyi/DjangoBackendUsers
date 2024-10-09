ad111491258502ed3586660696bbcfb1e8572d47
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2024-10-10 00:16:38 +0300

    Added View for User & Transaction CRUD

--- users/views.py
+++ users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 
-# Create your views here.
+from users.models import Transaction
+from users.serializers import UserSerializer, TransactionCreateSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({"id": user.id}, status=201)
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    lookup_field = "id"
+    lookup_url_kwarg = "user_id"
+
+
+class ListUserView(generics.ListAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAdminUser,)
+
+
+class TransactionCreateView(generics.CreateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionCreateSerializer
+    permission_classes = (IsAdminUser,)
